0a88298c9642fda963c0c03907572e64d7a51cbe
anuragmuthyam <anu.drumcoder@gmail.com>
2020-09-30 20:41:18 -0400

    implemented server side

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import {Header} from './components/header';
 import {AutomationPage} from './pages/automationPage';
 
-ReactDOM.render(
+ReactDOM.hydrate(
   <React.StrictMode>
     <Header />
     <AutomationPage />
